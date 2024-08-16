af7ed92e95d5e0b01bd7a546df71b05a606f3227
WX <123456>
2024-08-16 18:02:56 +0800

    feat: 项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<!-- 使用 type="home" 属性设置首页，其他页面不需要设置，默认为page；推荐使用json5，更强大，且允许注释 -->
+<route lang="json5" type="home">
+{
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '首页',
+  },
+}
+</route>
+<template>
+  <wd-navbar fixed placeholder title="首页" safeAreaInsetTop></wd-navbar>
+  <view
+    class="bg-white overflow-hidden pt-2 px-4"
+    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
+  >
+    <view class="text-center text-4xl main-title-color mt-4 test-unocss-apply">Rookie uniapp</view>
+    <view class="text-justify max-w-100 m-auto text-4 indent mb-2">{{ description }}</view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
+
+const description = ref(
+  'Rookie uniapp 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
+)
+
+onLoad(() => {})
+
+/** 激活“分享给好友” */
+onShareAppMessage((options: Page.ShareAppMessageOption): Page.CustomShareContent => {
+  console.log('options:', options)
+  return {
+    title: 'Rookie uniapp',
+    desc: 'Rookie uniapp 演示示例',
+    path: '/pages/index/index?id=xxx',
+  }
+})
+</script>
+
+<style>
+.main-title-color {
+  color: #d14328;
+}
+
+.test-unocss-apply {
+  @apply m-4;
+}
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,231 @@
+<route lang="json5" type="page">
+{
+  style: { navigationBarTitleText: '登录' },
+}
+</route>
+
+<!-- 蓝色登录页面2 -->
+<template>
+  <view style="height: 100vh; background: #fff">
+    <view class="img-a">
+      <view class="t-b">
+        您好，
+        <br />
+        欢迎使用，XXX小程序
+      </view>
+    </view>
+    <view class="login-view" style="">
+      <view class="t-login">
+        <form class="cl">
+          <view class="t-a">
+            <text class="txt">手机号</text>
+            <input
+              type="number"
+              name="phone"
+              placeholder="请输入您的手机号"
+              maxlength="11"
+              v-model="phone"
+            />
+          </view>
+          <view class="t-a">
+            <text class="txt">密码</text>
+            <input
+              type="password"
+              name="code"
+              maxlength="18"
+              placeholder="请输入您的密码"
+              v-model="pwd"
+            />
+          </view>
+          <button @tap="login()">登 录</button>
+          <view class="reg" @tap="reg()">注 册</view>
+        </form>
+        <view class="t-f"><text>—————— 第三方账号登录 ——————</text></view>
+        <view class="t-e cl">
+          <view class="t-g" @tap="wxLogin()">
+            <image src="https://zhoukaiwen.com/img/loginImg/wx.png"></image>
+          </view>
+          <view class="t-g" @tap="qqLogin()">
+            <image src="https://zhoukaiwen.com/img/loginImg/qq.png"></image>
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { currRoute } from '@/utils'
+import { useUserStore } from '@/store'
+const userStore = useUserStore()
+const phone = ref('')
+const pwd = ref('')
+const login = () => {
+  if (!phone.value) {
+    uni.showToast({ title: '请输入您的手机号', icon: 'none' })
+    return
+  }
+  if (!/^[1][3,4,5,7,8,9][0-9]{9}$/.test(that.phone)) {
+    uni.showToast({ title: '请输入正确手机号', icon: 'none' })
+    return
+  }
+  if (!pwd.value) {
+    uni.showToast({ title: '请输入您的密码', icon: 'none' })
+    return
+  }
+  uni.showToast({ title: '登录成功！', icon: 'none' })
+  userStore.setUserInfo({ nickname: '菲鸽', avatar: '', token: 'abcdef' })
+  const { query } = currRoute()
+  uni.redirectTo({ url: query.redirect })
+}
+// 注册按钮点击
+const reg = () => {
+  uni.showToast({ title: '注册跳转', icon: 'none' })
+}
+// 等三方微信登录
+const wxLogin = () => {
+  uni.showToast({ title: '微信登录', icon: 'none' })
+}
+// 第三方QQ登录
+const qqLogin = () => {
+  uni.showToast({ title: 'QQ登录', icon: 'none' })
+}
+onLoad((opt) => {
+  console.log('login onLoad', opt)
+})
+</script>
+<style lang="scss" scoped>
+.txt {
+  font-size: 32rpx;
+  font-weight: bold;
+  color: #333;
+}
+
+.img-a {
+  width: 100%;
+  height: 450rpx;
+  background-image: url('https://zhoukaiwen.com/img/loginImg/head.png');
+  background-size: 100%;
+}
+
+.reg {
+  height: 90rpx;
+  margin-top: 30rpx;
+  font-size: 28rpx;
+  font-weight: bold;
+  line-height: 90rpx;
+  color: #000;
+  text-align: center;
+  background: #f5f6fa;
+  border-radius: 50rpx;
+}
+
+.login-view {
+  position: relative;
+  width: 100%;
+  margin-top: -120rpx;
+  background-color: #fff;
+  border-radius: 6% 6% 0% 0;
+}
+
+.t-login {
+  width: 600rpx;
+  padding-top: 80rpx;
+  margin: 0 auto;
+  font-size: 28rpx;
+}
+
+.t-login button {
+  height: 90rpx;
+  font-size: 28rpx;
+  font-weight: bold;
+  line-height: 90rpx;
+  color: #fff;
+  background: #2796f2;
+  border-radius: 50rpx;
+}
+
+.t-login input {
+  height: 90rpx;
+  margin-bottom: 50rpx;
+  font-size: 28rpx;
+  line-height: 90rpx;
+  border-bottom: 1px solid #e9e9e9;
+}
+
+.t-login .t-a {
+  position: relative;
+}
+
+.t-b {
+  padding: 130rpx 0 0 70rpx;
+  font-size: 42rpx;
+  font-weight: bold;
+  line-height: 70rpx;
+  color: #fff;
+  text-align: left;
+}
+
+.t-login .t-c {
+  position: absolute;
+  top: 22rpx;
+  right: 22rpx;
+  height: 50rpx;
+  padding: 0 25rpx;
+  font-size: 24rpx;
+  line-height: 50rpx;
+  color: #fff;
+  background: #5677fc;
+  border-radius: 50rpx;
+}
+
+.t-login .t-d {
+  margin: 80rpx 0;
+  color: #999;
+  text-align: center;
+}
+
+.t-login .t-e {
+  width: 250rpx;
+  margin: 80rpx auto 0;
+  text-align: center;
+}
+
+.t-login .t-g {
+  float: left;
+  width: 50%;
+}
+
+.t-login .t-e image {
+  width: 50rpx;
+  height: 50rpx;
+}
+
+.t-login .t-f {
+  margin: 150rpx 0 0;
+  color: #666;
+  text-align: center;
+}
+
+.t-login .t-f text {
+  margin-left: 20rpx;
+  font-size: 27rpx;
+  color: #aaa;
+}
+
+.t-login .uni-input-placeholder {
+  color: #aeaeae;
+}
+
+.cl {
+  zoom: 1;
+}
+
+.cl::after {
+  display: block;
+  height: 0;
+  clear: both;
+  visibility: hidden;
+  content: '\20';
+}
+</style>
